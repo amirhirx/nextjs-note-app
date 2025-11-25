b64208c94f80bf0482cb3782a542d8641d5c85cd
AmirHossein Hosseinian <amirhirx@gmail.com>
2025-11-25 22:18:03 +0330

    Create EmptyNotes component

---
+++ components/EmptyNotes.tsx
@@ -0,0 +1,10 @@
+export default function EmptyTask() {
+    return (
+        <div className="md:w-10/12 h-screen flex justify-center items-center">
+            <div className="text-center">
+                <h2 className="text-xl font-extrabold">There is no notes</h2>
+                <p>Add a new note...</p>
+            </div>
+        </div>
+    )
+}
