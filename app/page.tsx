f9eb0bcc0ddb543ae6c8abf34604e94aea92c5ba
AmirHossein Hosseinian <amirhirx@gmail.com>
2025-11-25 22:18:59 +0330

    Add components to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,24 @@
-export default function Home() {
-    return <div></div>
+import EmptyTask from "@/components/EmptyNotes"
+import NewNoteForm from "@/components/NewNoteForm"
+import NoteCard from "@/components/NoteCard"
+import NotesContainer from "@/components/NotesContainer"
+import { getNotes } from "@/services/getNotes"
+
+export default async function Home() {
+    const notes = await getNotes()
+    return (
+        <div className="p-4 md:p-8 flex flex-col lg:flex-row lg:justify-around gap-4">
+            {notes && notes.length > 0 ? (
+                <NotesContainer>
+                    {notes.map(({ ...note }) => {
+                        return <NoteCard key={note.id} {...note} />
+                    })}
+                </NotesContainer>
+            ) : (
+                <EmptyTask />
+            )}
+
+            <NewNoteForm />
+        </div>
+    )
 }
