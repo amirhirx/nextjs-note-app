16c7a33e66d842e40c7faea07e4ea85cf0ebaccc
AmirHossein Hosseinian <amirhirx@gmail.com>
2025-11-25 21:41:46 +0330

    Move category api to api directory

--- services/getCategories.ts
+++ services/getCategories.ts
@@ -1,6 +1,6 @@
 import { ICategory } from "@/types/category"
 
-const url = "http://localhost:3000/category"
+const url = "http://localhost:3000/api/category"
 
 export async function getCategories() {
     const res = await fetch(url)
