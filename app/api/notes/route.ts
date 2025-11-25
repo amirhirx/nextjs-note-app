d64e43b164d3086676072dc85d7a56b979eaef17
AmirHossein Hosseinian <amirhirx@gmail.com>
2025-11-25 22:28:29 +0330

    Create an empty notes and replace that

--- app/api/notes/route.ts
+++ app/api/notes/route.ts
@@ -1,8 +1,11 @@
+import { INote } from "@/types/Note"
 import { NextResponse } from "next/server"
+
+const notes: INote[] = []
 
 export function GET() {
     try {
-        return NextResponse.json({ notes: [] }, { status: 200 })
+        return NextResponse.json({ notes }, { status: 200 })
     } catch (error) {
         return NextResponse.json({ error: error }, { status: 500 })
     }
