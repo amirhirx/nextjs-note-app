21367f4c7d8c5e6ca59afb99649968d8d181c302
AmirHossein Hosseinian <amirhirx@gmail.com>
2025-11-27 18:49:47 +0330

    Create a POST API for add notes

--- app/api/notes/route.ts
+++ app/api/notes/route.ts
@@ -3,9 +3,31 @@ import { NextResponse } from "next/server"
 
 const notes: INote[] = []
 
-export function GET() {
+export async function GET() {
     try {
         return NextResponse.json({ notes }, { status: 200 })
+    } catch (error) {
+        return NextResponse.json({ error: error }, { status: 500 })
+    }
+}
+
+export async function POST(req: Request) {
+    try {
+        const { title, content, category, createdAt } = await req.json()
+        const newNote: INote = {
+            id: String(Math.random() * 100),
+            title,
+            content,
+            category,
+            createdAt,
+        }
+
+        notes.push(newNote)
+
+        return NextResponse.json(
+            { message: "Your note added successfuly" },
+            { status: 200 }
+        )
     } catch (error) {
         return NextResponse.json({ error: error }, { status: 500 })
     }
